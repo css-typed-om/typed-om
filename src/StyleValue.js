1c7969412a84e7b58df2561d7947f88018355211
rjwright <rjwright@localhost>
2015-11-19 15:08:19 +1100

    fix indenting

--- src/StyleValue.js
+++ src/StyleValue.js
@@ -16,30 +16,28 @@ function StyleValue() {
 }
 
 StyleValue.prototype = {
-	parse: function(property, value) {
-		if (typeof value == 'string') {
-			nValue = Number.parseFloat(value);
-			if (nValue !== NaN) {
-				return new NumberValue(nValue);
+  parse: function(property, value) {
+    if (typeof value == 'string') {
+      nValue = Number.parseFloat(value);
+      if (nValue !== NaN) {
+			  return new NumberValue(nValue);
 			}
-			throw('not a number');
+		  throw('not a number');
 		}
 	}
 }
 
 function NumberValue(value) {
-	this.cssString = '' + value;
+  this.cssString = '' + value;
 	if (typeof value == 'number') {
-		this.value = value;
-	}
-	else if (typeof value == 'string') {
-		nValue = Number.parseFloat(value);
-		if (nValue !== NaN) {
-			this.value = nValue;
+	  this.value = value;
+	} else if (typeof value == 'string') {
+	  nValue = Number.parseFloat(value);
+	  if (nValue !== NaN) {
+		  this.value = nValue;
 		}
-	}
-	else {
-		throw('not a number');
+	} else {
+	  throw('not a number');
 	}
 }
 
