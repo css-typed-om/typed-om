60c458a8f74441eac6d6f518e03fcf5d44aeb056
rjwright <rjwright@chromium.org>
2015-11-24 15:46:59 +1100

    Add testcases for computed-style-property-map

--- test/js/computed-style-property-map.js
+++ test/js/computed-style-property-map.js
@@ -1,8 +1,33 @@
 suite('ComputedStylePropertyMap', function() {
+  setup(function() {
+    this.element = document.createElement('div');
+    document.documentElement.appendChild(this.element);
+    this.element.style.opacity = '0.5';
+  });
+  teardown(function() {
+    document.documentElement.removeChild(this.element);
+  });
+
   test('ComputedStylePropertyMap is a ComputedStylePropertyMap and a StylePropertyMap', function() {
-  	var element = document.createElement('div');
-    var computedStyleMap = new ComputedStylePropertyMap(element);
+    var computedStyleMap = new ComputedStylePropertyMap(this.element);
     assert.instanceOf(computedStyleMap, ComputedStylePropertyMap);
     assert.instanceOf(computedStyleMap, typedOMTesting.StylePropertyMap);
+  });
+
+  test('ComputedStylePropertyMap.get works for properties that can only be NumberValues', function() {
+    var computedStyleMap = new ComputedStylePropertyMap(this.element);
+    var opacity = computedStyleMap.get('opacity');
+    assert.instanceOf(opacity, NumberValue);
+    assert.equal(opacity.value, 0.5);
+    assert.equal(opacity.cssString, '0.5');
+  });
+
+  test('window.getComputedStyleMap works', function() {
+    var computedStyleMap = getComputedStyleMap(this.element);
+    assert.instanceOf(computedStyleMap, typedOMTesting.StylePropertyMap);
+    var opacity = computedStyleMap.get('opacity');
+    assert.instanceOf(opacity, NumberValue);
+    assert.equal(opacity.value, 0.5);
+    assert.equal(opacity.cssString, '0.5');
   });
 });
