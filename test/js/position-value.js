a132a865f2d18bd08ad7f8cd77860e4053f5a33c
Nick Lawton <lawton@lawton-z840-l.syd.corp.google.com>
2015-12-03 15:31:37 +1100

    Created unit tests for the PositionValue class

--- test/js/position-value.js
+++ test/js/position-value.js
@@ -1,3 +1,21 @@
 suite('PositionValue', function() {
-  
+  test('Constructor for Position value will throw a type error if either xPos or yPos is not an instance'
+      +'of the LengthValue class', function() {
+    assert.throws(function () {
+      new PositionValue(2, 3)
+      }, 'xPos is not of type: LengthValue');
+
+    assert.throws(function () {
+      new PositionValue(new SimpleLength(3, 'px'), 3)
+      }, 'yPos is not of type: LengthValue');
+  });
+
+  test('CssStrings created by an instance of the PositionValue class should be the xPos cssString followed '
+      +'by a space and the the yPos cssString', function() {
+    var lengthValue_1 = new CalcLength({px: 10, em: 3.2});
+    var lengthValue_2 = new SimpleLength(3, 'px');
+
+    var positionValue = new PositionValue(lengthValue_1, lengthValue_2);
+    assert.strictEqual(positionValue.cssString, lengthValue_1.cssString + " " + lengthValue_2.cssString);
+  });
 });
