dd516a7343e7abee8d682182cbc1db3e8a489e99
Nick Lawton <lawton@lawton-z840-l.syd.corp.google.com>
2015-12-03 13:54:25 +1100

    Added outline for the PositionValue class position-value.js has basic constructor outline but currently no implementation

---
+++ src/position-value.js
@@ -0,0 +1,32 @@
+// Copyright 2015 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+// limitations under the License.
+
+(function(shared, scope, testing) {
+
+  /**
+   * PositionValue(xPos, yPos)
+   */
+  function PositionValue(xPos, yPos) {
+    // Construct length values with x and y offsets
+
+    this.cssString = "";
+  }
+
+  PositionValue.prototype = Object.create(shared.StyleValue.prototype)
+
+   scope.PositionValue = PositionValue;
+  if (TYPED_OM_TESTING)
+    testing.PositionValue = PositionValue;
+
+})(baseClasses, window, typedOMTesting);
