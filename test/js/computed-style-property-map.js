5eaeaed03012f7d867944e5b2672e4e158884c35
rjwright <rjwright@chromium.org>
2015-12-04 16:33:00 +1100

    fix line length in test/js/computed-style-property-map

--- test/js/computed-style-property-map.js
+++ test/js/computed-style-property-map.js
@@ -8,13 +8,15 @@ suite('ComputedStylePropertyMap', function() {
     document.documentElement.removeChild(this.element);
   });
 
-  test('ComputedStylePropertyMap is a ComputedStylePropertyMap and a StylePropertyMap', function() {
+  test('ComputedStylePropertyMap is a ComputedStylePropertyMap and a ' +
+      'StylePropertyMap', function() {
     var computedStyleMap = new ComputedStylePropertyMap(this.element);
     assert.instanceOf(computedStyleMap, ComputedStylePropertyMap);
     assert.instanceOf(computedStyleMap, typedOMTesting.StylePropertyMap);
   });
 
-  test('ComputedStylePropertyMap.get works for properties that can only be NumberValues', function() {
+  test('ComputedStylePropertyMap.get works for properties that can only be ' +
+      'NumberValues', function() {
     var computedStyleMap = new ComputedStylePropertyMap(this.element);
     var opacity = computedStyleMap.get('opacity');
     assert.instanceOf(opacity, NumberValue);
