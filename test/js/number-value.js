9bd1f0472c468b94f4886e42ce96f32405e5630b
rjwright <rjwright@chromium.org>
2015-12-04 16:36:45 +1100

    Fix line lengths in test/js/number-value

--- test/js/number-value.js
+++ test/js/number-value.js
@@ -1,18 +1,24 @@
 suite('NumberValue', function() {
   test('NumberValue is a NumberValue and a StyleValue', function() {
     var numberVal = new NumberValue(3);
-    assert.instanceOf(numberVal, NumberValue, 'A new NumberValue should be an instance of NumberValue');
-    assert.instanceOf(numberVal, StyleValue, 'A new NumberValue should be an instance of StyleValue');
+    assert.instanceOf(numberVal, NumberValue,
+      'A new NumberValue should be an instance of NumberValue');
+    assert.instanceOf(numberVal, StyleValue,
+      'A new NumberValue should be an instance of StyleValue');
   });
 
-  test('NumberValue constructor throws exception for invalid values', function() {
+  test('NumberValue constructor throws exception for invalid values',
+      function() {
     assert.throws(function() {new NumberValue('a4')});
     assert.throws(function() {new NumberValue({})});
   });
 
-  test('NumberValue constructor works correctly for numbers and numeric strings', function() {
+  test('NumberValue constructor works correctly for numbers and numeric ' +
+      'strings', function() {
     var numberValFromString;
-    assert.doesNotThrow(function() {numberValFromString = new NumberValue('9.2')});
+    assert.doesNotThrow(function() {
+      numberValFromString = new NumberValue('9.2');
+    });
     assert.strictEqual(numberValFromString.cssString, '9.2');
     assert.strictEqual(numberValFromString.value, 9.2);
     var numberValFromNumber;
