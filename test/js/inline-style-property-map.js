52a9c3b0723a0f1a2d64f14e7698853a6e0a722e
Nick Lawton <lawton@lawton-z840-l.syd.corp.google.com>
2015-12-10 15:30:46 +1100

    Fixed small issues in Inline StylePropertyMaptest script

--- test/js/inline-style-property-map.js
+++ test/js/inline-style-property-map.js
@@ -1,16 +1,16 @@
 suite('Inline StylePropertyMap', function() {
   setup(function() {
     this.element = document.createElement('div');
-    document.documentElement.appendChild(this.element);
+    document.body.appendChild(this.element);
     this.element.style.opacity = '0.5';
   });
   teardown(function() {
-    document.documentElement.removeChild(this.element);
+    document.body.removeChild(this.element);
   });
 
   test('Test that a new InlineStylePropertyMap is created when a HTML element referance calls the ' +
 	    'styleMap method', function() {
-	 var inlineStyleMap = this.element.styleMap();
-	 assert.instanceOf(inlineStyleMap, StylePropertyMap, 'The styleMap method should return an instance of InlineStylePropertyMap');
+	  var inlineStyleMap = this.element.styleMap();
+	  assert.instanceOf(inlineStyleMap, StylePropertyMap, 'The styleMap method should return an instance of InlineStylePropertyMap');
   });
 });
