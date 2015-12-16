93a87f83853e4007b22a6356d1de39ced17b1627
Nick Lawton <lawton@lawton-z840-l.syd.corp.google.com>
2015-12-16 11:57:22 +1100

    Implemented the set method for the StylePropertyMap class This currently has only been implemented for the most specific longhand values i.e. ones that only support setting one value only LongHands that are set using multiple values and those that accept sequences of style values are currently unsupported

--- src/style-property-map.js
+++ src/style-property-map.js
@@ -20,7 +20,23 @@
   internal.inherit(StylePropertyMap, internal.StylePropertyMapReadOnly);
 
   StylePropertyMap.prototype.set = function(property, value) {
-    throw new TypeError('Function not implemented yet');
+    var cssPropertyDictionary = propertyDictionary();
+    if (!cssPropertyDictionary.isSupportedProperty(property)) {
+      throw new TypeError(property + 'Is not a Supported CSS property');
+    }
+
+    if (value instanceof StyleValue) {
+      if (!cssPropertyDictionary.isValidInput(property, value)) {
+        throw new TypeError('This StyleValue type cannot be set to this property');
+      }
+      this._styleObject[property] = value.cssString;
+      return;
+    }
+
+    if (value instanceof Array) {
+      throw new TypeError('Setting a sequence of StyleValues is not implemented yet');
+    }
+    throw new TypeError('The value you are setting must be a StyleValue object');
   };
 
   StylePropertyMap.prototype.append = function(property, value) {
--- test/js/inline-style-property-map.js
+++ test/js/inline-style-property-map.js
@@ -8,8 +8,38 @@ suite('Inline StylePropertyMap', function() {
     document.body.removeChild(this.element);
   });
 
-  test('Element.styleMap method returns a StylePropertyMap object', function() {
+  test('Element.styleMap method returns a StylePropertyMap object for that element', function() {
     var inlineStyleMap = this.element.styleMap();
-    assert.instanceOf(inlineStyleMap, StylePropertyMap, 'The styleMap method should return an instance of StylePropertyMap');
+    assert.instanceOf(inlineStyleMap, StylePropertyMap);
+  });
+
+  test('The set method should correctly set a supported StyleValue to a CSS style property of an HTML element', function() {
+    var inlineStyleMap = this.element.styleMap();
+    var simpleLength = new SimpleLength(9.2, 'percent');
+    inlineStyleMap.set('height', simpleLength);
+
+    assert.strictEqual(this.element.style['height'], simpleLength.cssString);
+  });
+
+  test('The set method should correctly set a supported KeywordValue to a CSS style property of an HTML element', function() {
+    var inlineStyleMap = this.element.styleMap();
+    var keyword = new KeywordValue('auto');
+    inlineStyleMap.set('height', keyword);
+
+    assert.strictEqual(this.element.style['height'], keyword.cssString);
+  });
+
+  test('The set method should throw a TypeError if a StyleValue unsupported by the CSS style property is set', function() {
+    var inlineStyleMap = this.element.styleMap();
+    var numberValue = new NumberValue(42);
+
+    assert.throw(function() {inlineStyleMap.set('height', numberValue)}, TypeError);
+  });
+
+  test('The set method should throw a TypeError if a KeywordValue unsupported by the CSS style property is set ', function() {
+    var inlineStyleMap = this.element.styleMap();
+    var keyword = new KeywordValue('lemon');
+
+    assert.throw(function() {inlineStyleMap.set('height', keyword)}, TypeError);
   });
 });
