4fb0f90669c8e940d602f2ffa1e99a56b1f38497
Anthony <thonywong@rocketmail.com>
2016-07-06 16:39:40 +1000

    Add blank lines

--- test/js/css-variable-reference-value.js
+++ test/js/css-variable-reference-value.js
@@ -1,16 +1,14 @@
 suite('CSSVariableReferenceValue', function() {
     test("The 'variable' attribute is correct", function() {
         var expected = 'anything';
-        var temp = new CSSVariableReferenceValue(expected);
-        assert.instanceOf(temp, CSSVariableReferenceValue,
+        var referenceValue = new CSSVariableReferenceValue(expected);
+        assert.instanceOf(referenceValue, CSSVariableReferenceValue,
             'A new CSSVariableReferenceValue should be an instance of CSSVariableReferenceValue');
-        assert.strictEqual(temp.variable, expected,
+        assert.strictEqual(referenceValue.variable, expected,
             'A new CSSVariableReferenceValue variable should be the same as its constructor');
     });
 
     test('Constructor does not throw', function() {
-        var expected = '12345';
-        var temp;
-        assert.doesNotThrow(function() { temp = new CSSVariableReferenceValue(expected); });  
+        assert.doesNotThrow(function() { new CSSVariableReferenceValue("12345"); });  
     })
 });
