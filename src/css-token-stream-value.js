5d945019118b44f1b5ccd14d949432e5862a3914
Anthony <thonywong@rocketmail.com>
2016-07-08 13:16:28 +1000

    Make CSSTokenStreamValue inherit CSSStyleValue

--- src/css-token-stream-value.js
+++ src/css-token-stream-value.js
@@ -5,12 +5,19 @@
             values = [];
         }
         if (!Array.isArray(values)) {
-            throw new TypeError('CSSTokenStreamValue should consist of an array of strings');
+            throw new TypeError('CSSTokenStreamValue should be an array of string');
+        }
+        for (var i = 0; i < values.length; i++) {
+            if (typeof values[i] != 'string') {
+                throw new TypeError("CSSTokenStreamValue's elements should be strings");
+            }
         }
         this.values = values;
-        // TODO: may need change like 
+        // TODO: may need to be changed like 
         //       https://drafts.css-houdini.org/css-typed-om/#tokenstreamvalue-normalization
     }
+
+    internal.inherit(CSSTokenStreamValue, CSSStyleValue);
 
     CSSTokenStreamValue.prototype.size = function() {
         return this.values.length;
--- test/js/css-token-stream-value.js
+++ test/js/css-token-stream-value.js
@@ -1,9 +1,15 @@
 suite('CSSTokenStreamValue', function() {
-    test("All of the values of the array are correct", function() {
-        var values = ['test', '12345', '1', 'a2', '2a'];
-        var tokenStreamValues = new CSSTokenStreamValue(values);
+    var values = ['test', '12345', '1', 'a2', '2a'];
+    var tokenStreamValues = new CSSTokenStreamValue(values);
+
+    test("CSSTokenStreamValue is a CSSTokenStreamValue and CSSStyleValue", function() {
         assert.instanceOf(tokenStreamValues, CSSTokenStreamValue,
             'A new CSSTokenStreamValue should be an instance of CSSTokenStreamValue');
+        assert.instanceOf(tokenStreamValues, CSSStyleValue,
+            'A new CSSTokenStreamValue should be an instance of CSSStyleValue');
+    });
+
+    test("All of the values of the array are correct", function() {
         assert.strictEqual(tokenStreamValues.size(), values.length,
             'A new CSSTokenStreamValue array\'s size must be the same as its constructor');
         for (var i = 0; i < values.length; ++i) {
@@ -15,5 +21,16 @@ suite('CSSTokenStreamValue', function() {
     test('Constructor does not throw', function() {
         assert.doesNotThrow(function() { new CSSTokenStreamValue(["12345"]); });
         assert.doesNotThrow(function() { new CSSTokenStreamValue(); });
-    })
+    });
+
+    test('Values not an array throws', function() {
+        assert.throw(function() { new CSSTokenStreamValue(1); }, TypeError, 'CSSTokenStreamValue should be an array of string');
+        assert.throw(function() { new CSSTokenStreamValue("123"); }, TypeError, 'CSSTokenStreamValue should be an array of string');
+        assert.throw(function() { new CSSTokenStreamValue({h:10, w:5, d:4, t:"5"});}, TypeError, 'CSSTokenStreamValue should be an array of string');
+    });
+
+    test('Values not an array of string throws', function() {
+        assert.throw(function() { new CSSTokenStreamValue([1]); }, TypeError, "CSSTokenStreamValue's elements should be strings");
+        assert.throw(function() { new CSSTokenStreamValue(["1234", "2342", 1]); }, TypeError, "CSSTokenStreamValue's elements should be strings");
+    });
 });
