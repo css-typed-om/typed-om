874f75f0b6fd6e1d9efa06e9ad8812bcd06be6bf
Anthony <thonywong@rocketmail.com>
2016-07-18 12:25:14 +1000

    Change tabsize to 2

--- test/js/css-variable-reference-value.js
+++ test/js/css-variable-reference-value.js
@@ -1,21 +1,21 @@
 suite('CSSVariableReferenceValue', function() {
-    test("The new CSSVariableReferenceValue attributes are correct", function() {
-        var expectedVariable = 'anything';
-        var expectedFallback = new CSSTokenStreamValue(["123"]);
-        var referenceValue = new CSSVariableReferenceValue(expectedVariable, expectedFallback);
-        assert.instanceOf(referenceValue, CSSVariableReferenceValue,
-            'A new CSSVariableReferenceValue should be an instance of CSSVariableReferenceValue');
-        assert.strictEqual(referenceValue.variable, expectedVariable,
-            'A new CSSVariableReferenceValue\'s variable should be the same as its constructor');
-        assert.strictEqual(referenceValue.fallback, expectedFallback,
-            'A new CSSVariableReferenceValue\'s fallback should be the same as its constructor');
-    });
+  test("The new CSSVariableReferenceValue attributes are correct", function() {
+    var expectedVariable = 'anything';
+    var expectedFallback = new CSSTokenStreamValue(["123"]);
+    var referenceValue = new CSSVariableReferenceValue(expectedVariable, expectedFallback);
+    assert.instanceOf(referenceValue, CSSVariableReferenceValue,
+      'A new CSSVariableReferenceValue should be an instance of CSSVariableReferenceValue');
+    assert.strictEqual(referenceValue.variable, expectedVariable,
+      'A new CSSVariableReferenceValue\'s variable should be the same as its constructor');
+    assert.strictEqual(referenceValue.fallback, expectedFallback,
+      'A new CSSVariableReferenceValue\'s fallback should be the same as its constructor');
+  });
 
-    test('Constructor only accepts a string and a CSSTokenStreamValue', function() {
-        assert.throw(function() { new CSSVariableReferenceValue(); }, TypeError, 'CSSVariableReferenceValue constructor should get two parameters');
-        assert.throw(function() { new CSSVariableReferenceValue("123"); }, TypeError, 'CSSVariableReferenceValue constructor should get two parameters');
-        assert.throw(function() { new CSSVariableReferenceValue(1234, 1234); }, TypeError, 'Variable of CSSVariableReferenceValue must be a string');
-        assert.throw(function() { new CSSVariableReferenceValue(["1"], 1234); }, TypeError, 'Variable of CSSVariableReferenceValue must be a string');
-        assert.throw(function() { new CSSVariableReferenceValue("123", 1234); }, TypeError, 'Fallback of CSSVariableReferenceValue must be a CSSTokenStreamValue');
-    });
+  test('Constructor only accepts a string and a CSSTokenStreamValue', function() {
+    assert.throw(function() { new CSSVariableReferenceValue(); }, TypeError, 'CSSVariableReferenceValue constructor should get two parameters');
+    assert.throw(function() { new CSSVariableReferenceValue("123"); }, TypeError, 'CSSVariableReferenceValue constructor should get two parameters');
+    assert.throw(function() { new CSSVariableReferenceValue(1234, 1234); }, TypeError, 'Variable of CSSVariableReferenceValue must be a string');
+    assert.throw(function() { new CSSVariableReferenceValue(["1"], 1234); }, TypeError, 'Variable of CSSVariableReferenceValue must be a string');
+    assert.throw(function() { new CSSVariableReferenceValue("123", 1234); }, TypeError, 'Fallback of CSSVariableReferenceValue must be a CSSTokenStreamValue');
+  });
 });
