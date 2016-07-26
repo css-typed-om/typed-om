5bbb126aee13b8fe3c87484d5d92c8ef8aaff0e8
Eddy Mead <meade@chromium.org>
2016-07-26 17:39:46 +1000

    Revert window change

--- src/css-transform-value.js
+++ src/css-transform-value.js
@@ -12,7 +12,7 @@
 //     See the License for the specific language governing permissions and
 // limitations under the License.
 
-(function(internal) {
+(function(internal, scope) {
 
   function computeMatrix(cssTransform) {
     if (!cssTransform.transformComponents.length) {
@@ -84,6 +84,6 @@
         valuesCallback.bind(this));
   };
 
-  window.CSSTransformValue = CSSTransformValue;
+  scope.CSSTransformValue = CSSTransformValue;
 
-})(typedOM.internal);
+})(typedOM.internal, window);
