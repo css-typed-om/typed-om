01dac0db65cedc0eb1d50ca0386cdcc0341c7c74
Anthony <thonywong@rocketmail.com>
2016-08-25 15:45:05 +1000

    Hide constructor for CSSImageValue

--- src/css-image-value.js
+++ src/css-image-value.js
@@ -1,13 +1,12 @@
 (function(internal, scope) {
 
-  function CSSImageValue(image) {
-    if (!(image instanceof Image)) {
-      throw new TypeError("image must be an Image object");
+  (function() {
+    function CSSImageValue() {
+      throw new TypeError("Can\'t instantiate CSSImageValue");
     }
-    this._image = image;
+    internal.inherit(CSSImageValue, CSSResourceValue);
 
-
-    function onLoad() {
+    CSSImageValue.prototype.onLoad = function() {
       this.state = "loaded";
       this.intrinsicWidth = this._image.naturalWidth;
       this.intrinsicHeight = this._image.naturalHeight;
@@ -15,33 +14,30 @@
         this.intrinsicRatio = this.intrinsicWidth / this.intrinsicHeight;
     }
 
-    function onError() {
+    CSSImageValue.prototype.onError = function() {
       this.state = "error";
     }
 
-    function onProgress() {
+    CSSImageValue.prototype.onProgress = function() {
       this.state = "loading";
     }
 
+    scope.CSSImageValue = CSSImageValue;
+  })();
+
+  var CSSImageValue = function(image) {
+    if (!(image instanceof Image)) {
+      throw new TypeError("image must be an Image object");
+    }
+    this._image = image;
     this.state = "unloaded";
     this.intrinsicWidth = null;
     this.intrinsicHeight = null;
     this.intrinsicRatio = null;
-
-    this._image.onload = onLoad.bind(this);
-    this._image.onprogress = onProgress.bind(this);
-    this._image.onerror = onError.bind(this);
   }
 
-  internal.inherit(CSSImageValue, CSSResourceValue);
+  CSSImageValue.prototype = Object.create(scope.CSSImageValue.prototype);
 
   internal.CSSImageValue = CSSImageValue;
-
-  (function() {
-    function CSSImageValue() {
-      throw new TypeError('Can\'t instantiate CSSImageValue');
-    }
-    scope.CSSImageValue = CSSImageValue;
-  })();
 
 })(typedOM.internal, window);
