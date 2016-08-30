c8833cb5e20057303b0da5c1262687b3ea205205
Anthony <thonywong@rocketmail.com>
2016-08-30 12:15:33 +1000

    Add CSSURLImageValue object

---
+++ test/js/css-url-image-value.js
@@ -0,0 +1,47 @@
+suite('CSSURLImageValue', function() {
+  test('CSSURLImageValue is a CSSURLImageValue, CSSImageValue, CSSResourceValue, and CSSStyleValue', function() {
+    assert.instanceOf(new CSSURLImageValue('resources/1x1-green.png'), CSSURLImageValue);
+    assert.instanceOf(new CSSURLImageValue('resources/1x1-green.png'), CSSImageValue);
+    assert.instanceOf(new CSSURLImageValue('resources/1x1-green.png'), CSSResourceValue);
+    assert.instanceOf(new CSSURLImageValue('resources/1x1-green.png'), CSSStyleValue);
+  });
+
+  test('CSSURLImageValue only accepts string', function() {
+    assert.throw(function() { new CSSURLImageValue(); }, TypeError, "URL must be a string");
+    assert.throw(function() { new CSSURLImageValue([]); }, TypeError, "URL must be a string");
+    assert.throw(function() { new CSSURLImageValue(1); }, TypeError, "URL must be a string");
+    assert.doesNotThrow(function() { new CSSURLImageValue(''); });
+  });
+
+  test('URL, state, and intrinsic dimensions of CSSURLImageValue object are correct', function(done) {
+    var urlImageValue = new CSSURLImageValue('resources/1x1-green.png');
+    assert.equal(urlImageValue.url, "resources/1x1-green.png");
+
+    var oldOnload = urlImageValue._image.onload;
+
+    urlImageValue._image.onload = function() {
+      oldOnload();
+      assert.strictEqual(urlImageValue.state, "loaded");
+      assert.strictEqual(urlImageValue.intrinsicWidth, 1);
+      assert.strictEqual(urlImageValue.intrinsicHeight, 1);
+      assert.strictEqual(urlImageValue.intrinsicRatio, 1);
+      done();
+    }
+  });
+
+  test('Invalid image will have error state and null intrinsic dimensions', function(done) {
+    var urlImageValue = new CSSURLImageValue('http://localhost');
+    assert.equal(urlImageValue.url, 'http://localhost');
+
+    var oldOnerror = urlImageValue._image.onerror;
+
+    urlImageValue._image.onerror = function() {
+      oldOnerror();
+      assert.strictEqual(urlImageValue.state, "error");
+      assert.strictEqual(urlImageValue.intrinsicWidth, null);
+      assert.strictEqual(urlImageValue.intrinsicHeight, null);
+      assert.strictEqual(urlImageValue.intrinsicRatio, null);
+      done();
+    };
+  });
+});
